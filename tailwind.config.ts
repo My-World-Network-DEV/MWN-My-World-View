aa64a0c7df3b1f48bd07e7ad3dcd8de58c7cacc2
My-World-Network-DEV <ffmyworldview@gmail.com>
2025-08-14 23:28:17 +1000

    chore(tailwind): update content paths in tailwind.config.ts for improved file matching; refactor import path in layout.tsx

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,12 @@
 import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: ['./src/**/*.{ts,tsx}'],
+  content: [
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/features/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
   theme: {
     extend: {},
   },
